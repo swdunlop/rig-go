a5899f3a7e72bbe0cfed18cc8c18a4327c906b9a
Scott W. Dunlop <swdunlop@gmail.com>
2024-02-19 17:18:06 -0800

    remove rig/www from run.go

--- run.go
+++ run.go
@@ -7,7 +7,6 @@ import (
 	"github.com/swdunlop/rig-go/rig"
 	"github.com/swdunlop/rig-go/rig/esbuild"
 	"github.com/swdunlop/rig-go/rig/golang"
-	"github.com/swdunlop/rig-go/rig/www"
 	"github.com/swdunlop/zugzug-go"
 	"github.com/swdunlop/zugzug-go/zug/parser"
 )
@@ -30,11 +29,7 @@ func runRig(ctx context.Context) error {
 		}
 		options = append(options, esbuild.Rig(
 			esbuild.EntryPoint(esbuildFile),
-			esbuild.OutDir(wwwDir),
 		))
-	}
-	if wwwDir != "" {
-		options = append(options, www.Rig(wwwDir))
 	}
 	if golangPkg != "" {
 		options = append(options, golang.Rig(golangPkg))
